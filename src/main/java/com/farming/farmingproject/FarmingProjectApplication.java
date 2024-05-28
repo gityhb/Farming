98f5d76db8a52b8ab1aa2e3fc988a7dcc5a42fc0
SoJeong <misteryoh0308@gmail.com>
2024-05-28 16:02:24 +0900

    init commit

--- src/main/java/com/farming/farmingproject/FarmingProjectApplication.java
+++ src/main/java/com/farming/farmingproject/FarmingProjectApplication.java
@@ -20,9 +20,10 @@ class SlideController {
 	@GetMapping("/api/slides")
 	public String[][] getSlides() {
 		return new String[][]{
-				{"Slide 1 - Text 1", "Slide 1 - Text 2", "Slide 1 - Text 3"},
-				{"Slide 2 - Text 1", "Slide 2 - Text 2", "Slide 2 - Text 3"},
-				{"Slide 3 - Text 1", "Slide 3 - Text 2", "Slide 3 - Text 3"}
+				{"2024.04.30(TUES)", "FARMING 파밍", "저희는 직거래 국내산 농산물만 취급합니다.\n" +
+						"신선하고 다양한 농산물을 만나보세요!"},
+				{"2024.04.30(TUES)", "특가 타임 세일", "16:00 - 17:00"},
+				{"오늘의 추천", "Slide 3 - Text 2", "Slide 3 - Text 3"}
 		};
 	}
 }
