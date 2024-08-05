7b9ab820f5784c4cf3b9dfb4f6820f8f49a2586c
SoJeong <misteryoh0308@gmail.com>
2024-08-05 19:34:53 +0900

    08/05 19:34 OSJ

--- src/main/farmingproject/src/farmer_product_apply.js
+++ src/main/farmingproject/src/farmer_product_apply.js
@@ -10,7 +10,10 @@ function Farmer_product_apply() {
                         <h1>상품 등록 신청</h1>
                     </div>
                     <div id={'product_apply_img'}>
+                        <div className={'product_apply_img_box'}>
 
+                        </div>
+                        <div className={'product_apply_img_btn'}>사진첨부</div>
                     </div>
                     <form>
                         <div className={'farmer_product_apply_form_group'}>
@@ -28,6 +31,7 @@ function Farmer_product_apply() {
                                     판매자
                                 </div>
                                 <input
+                                    className={'pform_seller'}
                                     type="text"
                                     placeholder="판매자 이름 쓸 예정"
                                     readOnly
@@ -38,13 +42,25 @@ function Farmer_product_apply() {
                                     가격
                                 </div>
                                 <input
-                                    type="text"
-                                    placeholder="20자 이내로 입력해주세요"
+                                    type="number"
+                                    placeholder="0"
                                 />
+                                <select>
+                                    <option value={'g'} selected>g</option>
+                                    <option value={'kg'}>kg</option>
+                                    <option value={'개'}>개</option>
+                                    <option value={'팩'}>팩</option>
+                                    <option value={'박스'}>박스</option>
+                                </select>
+                                <input
+                                    type="number"
+                                    placeholder="0"
+                                />
+                                <span> 원</span>
                             </div>
                             <div className={'pform_group'}>
                                 <div className={'pform_title'}>
-                                    원산지
+                                원산지
                                 </div>
                                 <input
                                     type="text"
@@ -61,13 +77,23 @@ function Farmer_product_apply() {
                                     <option value={'next'}>내일</option>
                                     <option value={'always'}>상시배송</option>
                                 </select>
-                                <span>※ 상시배송 선택시 예상날짜를 상품정보란에 기입해야 합니다</span>
+                                <span className={'warning_red'}>※ 상시배송 선택시 예상날짜를 상품정보란에 기입해야 합니다.</span>
                             </div>
                             <div className={'pform_group'}>
                                 <div className={'pform_title'}>
-                                    원산지
+                                    상품정보
                                 </div>
-                                <textarea />
+                                <textarea className={'pfrom_product_info'} placeholder={'자세한 상품소개, 예상 배송날짜, 판매자 소개는 판매에 큰 도움이 됩니다 :)'}/>
+                            </div>
+                            <div className={'warning_gray'}>
+                                ※승인 결과는 신청 후 영업일 4~6일 이내 문자로 발송됩니다.<br/><br/>
+
+                                ※허위 상품 및 법적 문제 있는 상품 등록시 강제 탈퇴될 수 있습니다.<br/><br/>
+
+                                ※문의전화 1588-1234
+                            </div>
+                            <div className={'pform_btn'}>
+                                <button type={'submit'}>신청하기</button>
                             </div>
                         </div>
                     </form>
