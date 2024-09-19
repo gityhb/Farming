10e77f99b2aea7254d35bdb7052d9c3024812882
soobin0205 <ss1817278@gmail.com>
2024-09-19 21:09:24 +0900

    09/19 ssb

--- src/main/farmingproject/src/auction.js
+++ src/main/farmingproject/src/auction.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import './auction.css';
 import './common/root.css';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-function Auction(){
-
-    return(
+function Auction() {
+    return (
         <div id="body">
             <div id={'auction_page'} className={'page'}>
                 <div id={'contents'}>
@@ -19,64 +18,55 @@ function Auction(){
                             <div className={'auction_content_item'}>
                                 <div className={'item_sec_1'}>08/05 13:00 예정</div>
                                 <div className={'item_sec_2'}>
-                                    <img src={"img/peach_1.jpg"} alt="상품 사진"/>
+                                    <img src={"img/peach_1.jpg"} alt="상품 사진" />
                                     <div className={'item_name'}>못나니 딱 복숭아 40kg</div>
                                     <div className={'item_minbid'}>최소 입찰가 | 200,000원</div>
                                 </div>
                             </div>
-                            <Link to={"/auction_detail"}>
-                            <div className={'auction_content_item'}>
-                                <div className={'item_sec_1'}>08/02 13:00 예정</div>
-                                <div className={'item_sec_2'}>
-                                    <img src={"img/watermelon_3.png"} alt="상품 사진"/>
-                                    <div className={'item_name'}>프리미엄 고당도 꿀수박 50통</div>
-                                    <div className={'item_minbid'}>최소 입찰가 | 340,000원</div>
 
-                                </div>
-                                <div className="auction_overlay">경매중</div>
+                            <div className={'auction_content_item'}>
+                                <Link to={"/auction_detail"}>
+                                    <div className={'item_sec_1'}>08/02 13:00 예정</div>
+                                    <div className={'item_sec_2'}>
+                                        <img src={"img/watermelon_3.png"} alt="상품 사진" />
+                                        <div className={'item_name'}>프리미엄 고당도 꿀수박 50통</div>
+                                        <div className={'item_minbid'}>최소 입찰가 | 340,000원</div>
+                                    </div>
+                                    <div className="auction_overlay">경매중</div> {/* 검정색 반투명 경매중 오버레이 */}
+                                </Link>
                             </div>
-                            </Link>
+
                             <div className={'auction_content_item'}>
                                 <div className={'item_sec_1'}>08/05 13:00 예정</div>
                                 <div className={'item_sec_2'}>
-                                    <img src={"img/plum.png"} alt="상품 사진"/>
+                                    <img src={"img/plum.png"} alt="상품 사진" />
                                     <div className={'item_name'}>과즙 가득한 꿀자두 15kg</div>
                                     <div className={'item_minbid'}>최소 입찰가 | 100,000원</div>
                                 </div>
                             </div>
-                        </div>
-                        <div className={'auction_content'}>
                             <div className={'auction_content_item'}>
                                 <div className={'item_sec_1'}>08/05 13:00 예정</div>
                                 <div className={'item_sec_2'}>
-                                    <img src={"img/greenapple.png"} alt="상품 사진"/>
+                                    <img src={"img/greenapple.png"} alt="상품 사진" />
                                     <div className={'item_name'}>달달한 아오리사과 15kg</div>
                                     <div className={'item_minbid'}>최소 입찰가 | 100,000원</div>
                                 </div>
                             </div>
+                        </div>
+
+                        <div className={'auction_content'}>
                             <div className={'auction_content_item'}>
                                 <div className={'item_sec_1'}>09/17 15:00 예정</div>
                                 <div className={'item_sec_2'}>
-                                    <img src={"img/tangerine.png"} alt="상품 사진"/>
+                                    <img src={"img/tangerine.png"} alt="상품 사진" />
                                     <div className={'item_name'}>제주도 서귀포시 감귤 30kg</div>
                                     <div className={'item_minbid'}>최소 입찰가 | 230,000원</div>
                                 </div>
                             </div>
                             <div className={'auction_content_item'}>
-                                <div className={'item_sec_1'}>08/05 13:00 예정</div>
-                                <div className={'item_sec_2'}>
-                                    <img src={"img/peach_1.jpg"} alt="상품 사진"/>
-                                    <div className={'item_name'}>못나니 딱 복숭아 40kg</div>
-                                    <div className={'item_minbid'}>최소 입찰가 | 200,000원</div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className={'auction_content'}>
-                            <div className={'auction_content_item'}>
                                 <div className={'item_sec_1'}>07/05 13:00 예정</div>
                                 <div className={'item_sec_2'}>
-                                    <img src={"img/carrot_2.png"} alt="상품 사진"/>
+                                    <img src={"img/carrot_2.png"} alt="상품 사진" />
                                     <div className={'item_name'}>아삭한 당근 20상자</div>
                                     <div className={'item_minbid'}>최소 입찰가 | 170,000원</div>
                                 </div>
@@ -85,7 +75,7 @@ function Auction(){
                             <div className={'auction_content_item'}>
                                 <div className={'item_sec_1'}>10/05 13:00 예정</div>
                                 <div className={'item_sec_2'}>
-                                    <img src={"img/cabbage.png"} alt="상품 사진"/>
+                                    <img src={"img/cabbage.png"} alt="상품 사진" />
                                     <div className={'item_name'}>아삭아삭 겉절이 배추 30kg</div>
                                     <div className={'item_minbid'}>최소 입찰가 | 100,000원</div>
                                 </div>
@@ -93,19 +83,17 @@ function Auction(){
                             <div className={'auction_content_item'}>
                                 <div className={'item_sec_1'}>07/05 13:00 예정</div>
                                 <div className={'item_sec_2'}>
-                                    <img src={"img/watermelon.png"} alt="상품 사진"/>
+                                    <img src={"img/watermelon.png"} alt="상품 사진" />
                                     <div className={'item_name'}>딱 주스용 수박 20kg</div>
                                     <div className={'item_minbid'}>최소 입찰가 | 130,000원</div>
                                 </div>
                                 <div className="auction_overlay">경매 완료</div>
                             </div>
                         </div>
-
                     </div>
                 </div>
             </div>
         </div>
-
     );
 }
 
--- src/main/farmingproject/src/auction_detail.js
+++ src/main/farmingproject/src/auction_detail.js
@@ -3,11 +3,20 @@ import './auction_detail.css';
 import './common/root.css';
 
 function AuctionDetail() {
-    const [timeLeft, setTimeLeft] = useState(300); // 5분 (300초)
+    const [timeLeft, setTimeLeft] = useState(5); // 5분 (300초)
+    const [isAuctionOver, setIsAuctionOver] = useState(false);
 
     useEffect(() => {
         const timer = setInterval(() => {
-            setTimeLeft((prevTime) => (prevTime > 0 ? prevTime - 1 : 0));
+            setTimeLeft((prevTime) => {
+                if (prevTime > 0) {
+                    return prevTime - 1;
+                } else {
+                    clearInterval(timer);
+                    setIsAuctionOver(true); // 경매가 종료되었음을 표시
+                    return 0;
+                }
+            });
         }, 1000);
 
         return () => clearInterval(timer);
@@ -43,18 +52,72 @@ function AuctionDetail() {
 
                     <div className="auction_detail_separator"></div>
 
-                    <div className="auction_amount">
-                        <div className="auction_amount_input">
-                            <div className="auction_time"> 1차 {formatTime(timeLeft)}</div>
-                            <div className={'auction_input_container'}>
-                                <input type={'number'} className={'auction_amount_input_field'} placeholder={"입찰가 입력"}/>
-                                <div className="auction_amount_text"> 원</div>
+                    {isAuctionOver ? (
+                        <>
+                            <div className="auction_amount">
+                                <div className="auction_complete_input">
+                                    <div className="auction_complete_box"></div>
+                                    <div className="auction_complete_text">경매완료</div>
+                                </div>
+                                <div className="auction_amount_btn">입찰하기</div>
                             </div>
+
+                            <div className="auction_graphs_container">
+                                <div className="graph_section">
+                                    <div className="graph_title">1차 입찰 결과 TOP5</div>
+                                    <div className="graph">
+                                        <div className="bar bar1">
+                                            <div className="bar_value">400,000</div>
+                                        </div>
+                                        <div className="bar bar2">
+                                            <div className="bar_value">397,000</div>
+                                        </div>
+                                        <div className="bar bar3">
+                                            <div className="bar_value">395,000</div>
+                                        </div>
+                                        <div className="bar bar4">
+                                            <div className="bar_value">394,000</div>
+                                        </div>
+                                        <div className="bar bar5">
+                                            <div className="bar_value">388,000</div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="graph_section">
+                                    <div className="graph_title">1차 + 2차 입찰 결과 TOP5</div>
+                                    <div className="graph">
+                                        <div className="bar bar1">
+                                            <div className="bar_value">470,000</div>
+                                        </div>
+                                        <div className="bar bar2">
+                                            <div className="bar_value">450,000</div>
+                                        </div>
+                                        <div className="bar bar3">
+                                            <div className="bar_value">435,000</div>
+                                        </div>
+                                        <div className="bar bar4">
+                                            <div className="bar_value">400,000</div>
+                                        </div>
+                                        <div className="bar bar5">
+                                            <div className="bar_value">397,000</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </>
+                    ) : (
+                        <div className="auction_amount">
+                            <div className="auction_amount_input">
+                                <div className="auction_time"> 1차 {formatTime(timeLeft)}</div>
+                                <div className={'auction_input_container'}>
+                                    <input type={'number'} className={'auction_amount_input_field'}
+                                           placeholder={"입찰가 입력"}/>
+                                    <div className="auction_amount_text"> 원</div>
+                                </div>
+                            </div>
+                            <div className="auction_amount_btn">입찰하기</div>
                         </div>
-                        <div className="auction_amount_btn">
-                            입찰하기
-                        </div>
-                    </div>{/*auction_amount*/}
+                    )}
                 </div>
             </div>
         </div>
